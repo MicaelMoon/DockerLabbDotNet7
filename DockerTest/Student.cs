28a2ed7a73ac8335573fe19c2d65182501d43238
Micael <sjodin.micael.gmail.com>
2024-06-10 16:52:10 +0200

    Add project files.

---
+++ DockerTest/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace DockerTest
+{
+	public class Student
+	{
+		public string Id { get; set; } = Guid.NewGuid().ToString();
+		public string Name { get; set; }
+		public string Email { get; set; }
+	}
+}
